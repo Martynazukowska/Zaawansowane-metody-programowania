7964a43da098390a2c6c22966ac69305df2f99e8
Martyna Zukowska <martynazukowska7@interia.pl>
2022-11-07 15:26:39 +0100

    Plus pliki do wtyczek

---
+++ plugin/src/Interp4Rotate.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include "Interp4Rotate.hh"
+#include "MobileObj.hh"
+
+using std::cout;
+using std::endl;
+
+
+extern "C" {
+ Interp4Command* CreateCmd(void);
+  const char* GetCmdName() { return "Rotate"; }
+}
+
+
+
+
+/*!
+ * \brief
+ *
+ *
+ */
+Interp4Command* CreateCmd(void)
+{
+  return Interp4Rotate::CreateCmd();
+}
+
+
+/*!
+ *
+ */
+Interp4Rotate::Interp4Rotate(): _Speed_mmS(0)
+{}
+
+
+/*!
+ *
+ */
+void Interp4Rotate::PrintCmd() const
+{
+  /*
+   *  Tu trzeba napisać odpowiednio zmodyfikować kod poniżej.
+   */
+  cout << GetCmdName() << " " << _Speed_mmS  << " 10  2" << endl;
+}
+
+
+/*!
+ *
+ */
+const char* Interp4Rotate::GetCmdName() const
+{
+  return ::GetCmdName();
+}
+
+
+/*!
+ *
+ */
+bool Interp4Rotate::ExecCmd( MobileObj  *pMobObj,  int  Socket) const
+{
+  /*
+   *  Tu trzeba napisać odpowiedni kod.
+   */
+  return true;
+}
+
+
+/*!
+ *
+ */
+bool Interp4Rotate::ReadParams(std::istream& Strm_CmdsList)
+{
+  /*
+   *  Tu trzeba napisać odpowiedni kod.
+   */
+  return true;
+}
+
+
+/*!
+ *
+ */
+Interp4Command* Interp4Rotate::CreateCmd()
+{
+  return new Interp4Rotate();
+}
+
+
+/*!
+ *
+ */
+void Interp4Rotate::PrintSyntax() const
+{
+  cout << "   Rotate  NazwaObiektu  Szybkosc[m/s]  DlugoscDrogi[m]" << endl;
+}
